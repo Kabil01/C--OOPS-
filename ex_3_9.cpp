80b981140819c4f6b506e85b64a775a9cf43ea3f
Kabil01 <118066026+Kabil01@users.noreply.github.com>
2023-02-22 21:41:57 +0530

    Update ex_3_9.cpp

--- ex_3_9.cpp
+++ ex_3_9.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+string substring(string a,int b,int c){
+	int j=0;
+	char substr[c]={'\0'};
+	for(int i=b;i<b+c;i++){
+		substr[j]=a[i];
+		j++;
+	}
+	return substr;
+}
+
 int main(){
-	string inp;
+	string inp,out;
+	int pos,len;
 	cout<<"Enter the string :";
-	cin>>inp;
-	
+	getline(cin,inp);
+	cout<<"Position :";
+	cin>>pos;
+	cout<<"Length :";
+	cin>>len;
+	out=substring(inp,pos,len);
+	cout<<"Output :"<<out<<endl;
 }
